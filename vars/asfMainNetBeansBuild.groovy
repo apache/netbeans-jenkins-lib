b6a84b48c3bad4a12923adbbcc09029a1a49284d
Eric Barboni <skygo@apache.org>
2020-03-12 00:18:23 +0100

    revert regexp to older versio

--- vars/asfMainNetBeansBuild.groovy
+++ vars/asfMainNetBeansBuild.groovy
@@ -177,7 +177,8 @@ def call(Map params = [:]) {
                 }
                 when {
                     allOf {
-                        branch pattern : "release\\d+",comparator:"REGEXP"
+                        expression { BRANCH_NAME ==~ /release\/[0-9]+/ }
+                        //branch pattern : "release\\d+",comparator:"REGEXP" wait for modern 1.4.1
                         expression { month !='Invalid'}
                     }
                     
@@ -203,7 +204,8 @@ def call(Map params = [:]) {
                 }
                 when {
                     allOf {
-                        branch pattern : "release\\d+",comparator:"REGEXP"
+                        expression { BRANCH_NAME ==~ /release\/[0-9]+/ }
+                        //branch pattern : "release\\d+",comparator:"REGEXP" wait for modern 1.4.1
                         expression { month =='Invalid' }
                     }
                 }
