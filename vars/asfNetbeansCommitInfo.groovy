eb069c4f04da9f31de4ba4595fbfe2de0f9d7abb
Hector Espert <hectorespertpardo@gmail.com>
2020-09-17 19:16:04 +0200

    Create asfNetbeansCommitInfo.groovy

---
+++ vars/asfNetbeansCommitInfo.groovy
@@ -0,0 +1,73 @@
+#!groovy
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+ 
+def call(Map params = [:]) {
+    pipeline {
+        agent { node { label 'ubuntu' } }
+        stages {
+            stage("Commit Summary comment") {
+                when { changeRequest() }
+                steps {
+                    script {
+                        // GitHub requires new lines in comments to be windows style
+                        String NL = "\r\n";
+                   
+                        // Mark the comments we create
+                        String MARKER_COMMENT = "<!-- Autocomment Commit Summary Bot -->"
+                        
+                        //Message start
+                        String message = MARKER_COMMENT + NL
+                                        
+                        message += "Public Information PR requestor: ${env.CHANGE_AUTHOR_DISPLAY_NAME} \\<${env.CHANGE_AUTHOR_EMAIL}\\>" + NL //TODO: Get author email
+                                    
+                        message += "Summary generation date: ${sh(returnStdout: true, script: "date '+%Y-%m-%d %H:%M:%S'").trim()}" + NL
+                        
+                        // https://github.com/jenkinsci/pipeline-github-plugin#commit
+                        
+                        for (commit in pullRequest.commits) {
+                            message += "${commit.author}" + NL
+                        }
+                        
+                        // Check the existing comments of the PR, which are marked to come from this job
+                        def jobComments = []
+                        for (comment in pullRequest.comments) {
+                            if (comment.body.contains(MARKER_COMMENT)) {
+                                jobComments << comment
+                            }
+                        }
+                        
+                        if(jobComments.size() == 0) {
+                            // If there is not existing bot comment, add a new comment with the summary
+                            pullRequest.comment(message)
+                        } else {
+                            // If there is already one bot comment, the first bot comment is updated and the further bot comments are removed
+                            pullRequest.editComment(jobComments[0].id, message)
+                            for(int i = 1; i < jobComments.size(); i++) {
+                                pullRequest.deleteComment(jobComments[i].id)
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
