a2e7b9eb22c4f4ea911fdbe36c11ae3fa7ea9021
Eric Barboni <skygo@apache.org>
2020-10-19 12:05:54 +0200

    migrate to new tools description

--- vars/asfStandardBuild.groovy
+++ vars/asfStandardBuild.groovy
@@ -29,10 +29,10 @@ def call(Map params = [:]) {
         ])
 
     // now determine params
-    def jdk = params.containsKey('jdk') ? params.jdk : 'JDK 1.8 (latest)'
+    def jdk = params.containsKey('jdk') ? params.jdk : 'jdk_1.8_latest'
     // use the cmdLine parameter otherwise default depending on current branch
     def cmdline = params.containsKey('cmdline') ? params.cmdline : (env.BRANCH_NAME == 'master'?"clean deploy site:jar":"clean install")
-    def mvnName = params.containsKey('mvnName') ? params.mvnName : 'Maven 3.5.4'
+    def mvnName = params.containsKey('mvnName') ? params.mvnName : 'maven_3.5.4'
     def xvfb = params.containsKey('xvfb') ? true : false
 
     def defaultPublishers = [artifactsPublisher(disabled: false), junitPublisher(ignoreAttachments: false, disabled: false),
