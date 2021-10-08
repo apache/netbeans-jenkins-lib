6d50b04a0fbb9bb71ab4a3aa89253a33e0a7cb43
Eric Barboni <skygo@apache.org>
2021-10-08 16:48:03 +0200

    cleanws and add some steps

--- jobs/Jenkinsmatrixfile.groovy
+++ jobs/Jenkinsmatrixfile.groovy
@@ -1,36 +1,37 @@
 pipeline {
     options {
         buildDiscarder(logRotator(numToKeepStr: '2'))
-        disableConcurrentBuilds() 
+        disableConcurrentBuilds()
     }
     agent { node { label 'ubuntu' } }
-    
+
     stages {
-       
+
         stage("clone and prepare build") {
             tools {
                 jdk 'jdk_1.8_latest'
                 ant 'ant_latest'
-            } 
+            }
             steps {
-           
-     
+
+
                 git 'https://github.com/apache/netbeans'
                 sh 'rm -f *.json* '
                 sh 'rm -f nbbuild/NetBeans-dev-Netbeans/*.zip'
                 sh 'wget -Onetbeansrelease.json "https://gitbox.apache.org/repos/asf?p=netbeans-jenkins-lib.git;a=blob_plain;f=meta/netbeansrelease.json" '
-                sh 'ant build -Dmetabuild.jsonurl=file:netbeansrelease.json'
+                sh "ant build -Dmetabuild.jsonurl=file:netbeansrelease.json -Dzip.dir=${WORKSPACE}/zip"
+                sh "cd ${WORKSPACE}/zip/ && cd * && mv *.zip ${WORKSPACE}/zip/NetBeansIDE.zip "
+                stash includes: "zip/NetBeansIDE.zip", name: 'idebuildzip'
                 sh 'ant build-test-dist -Dmetabuild.jsonurl=file:netbeansrelease.json'
                 stash includes: 'nbbuild/build/testdist.zip', name: 'testbuildzip'
-                sh 'mv nbbuild/NetBeans-dev-Netbeans/netbeans*.zip nbbuild/NetBeansIDE.zip '
-                stash includes: 'nbbuild/NetBeansIDE.zip', name: 'idebuildzip'
+
             }
         }
-        stage ("EffectiveMatrix") 
+        stage ("EffectiveMatrix")
         {
             matrix {
-                agent { node { label 'ubuntu' } } 
-            
+                agent { node { label 'ubuntu' } }
+
                 axes {
                     axis {
                         name 'JDK'
@@ -38,14 +39,14 @@ pipeline {
                     }
                     axis {
                         name 'MODULE'
-                        values 'platform', 'ide' 
+                        values 'platform', 'ide'
                     }
                 }
                 stages {
                     stage('test') {
                         tools {
                             ant 'ant_latest'
-                        } 
+                        }
                         steps {
                             withAnt(jdk:"${env.JDK}") {
                                 unstash 'testbuildzip'
@@ -55,16 +56,22 @@ pipeline {
                                 sh 'java -version'
                                 sh 'ant -version'
                                 // this is not finished
-                                sh "ant -f ${WORKSPACE}/testdist/build.xml -Dtest.clusters=${env.MODULE} -Dtest.types=unit -Dnetbeans.dest.dir=${WORKSPACE}/netbeans"
-                                sh "ant -f ${WORKSPACE}/testdist/build.xml -Dtest.clusters=${env.MODULE} -Dtest.types=qa-functional -Dnetbeans.dest.dir=${WORKSPACE}/netbeans"
-                                
+                                sh "ant -f ${WORKSPACE}/testdir/build.xml -Dtest.clusters=${env.MODULE} -Dtest.types=unit -Dnetbeans.dest.dir=${WORKSPACE}/netbeans/netbeans"
+                                junit "${WORKSPACE}/testdir/unit/results/**/*.xml"
+                                //sh "ant -f ${WORKSPACE}/testdir/build.xml -Dtest.clusters=${env.MODULE} -Dtest.types=qa-functional -Dnetbeans.dest.dir=${WORKSPACE}/netbeans/netbeans"
+
                             }
                         }
-                        
+
                     }
-                }}
-        } 
-        
+                }
+
+            }
+        }
     }
-    
+    post {
+            cleanup {
+                cleanWs disableDeferredWipeout: true, deleteDirs: true
+            }
+    }
 }
