0d2abba3f429211a13698c099b162772003511c5
Eric Barboni <skygo@apache.org>
2022-07-13 17:28:19 +0200

    fix path for checking javadoc

--- vars/asfMainNetBeansBuild.groovy
+++ vars/asfMainNetBeansBuild.groovy
@@ -366,7 +366,7 @@ def doParallelClusters(cconfigs) {
                                 sh "cp -r build-${clustername}-temp/nbbuild/nbms/** dist${versionnedpath}nbms/"
 
                                 archiveArtifacts 'WEBZIP.zip'
-                                junit 'nbbuild/build/javadoc/checklinks-errors.xml'
+                                junit "build-${clustername}-temp/nbbuild/build/javadoc/checklinks-errors.xml"
                                 def localRepo = ".repository"
                                 def netbeansbase = "build-${clustername}-temp/nbbuild"
                                 withMaven(maven:tooling.myMaven,jdk:tooling.jdktool,publisherStrategy: 'EXPLICIT',mavenLocalRepo: localRepo,options:[artifactsPublisher(disabled: true)])
